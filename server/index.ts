e9c0c36f0e1cc3c53dea1de439f084ed6d5f246d
johnmoormaniii <johnmoormaniii@gmail.com>
2023-05-07 12:38:45 -0400

    progress

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,9 @@ import express, { Express, Request, Response, NextFunction } from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
+import passport from 'passport';
+import DiscordStrategy from 'passport-discord';
+import session from 'express-session';
 
 // route imports
 import authRouter from './routes/authRouter.js';
@@ -10,6 +13,44 @@ import apiRouter from './routes/apiRouter.js';
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: 'none',
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24, // One day
+    },
+  })
+);
+
+// const discordScopes = ['identify', 'email', 'guilds', 'guilds.join'];
+
+// passport.serializeUser((user, done) => {
+//   return done(null, user);
+// });
+
+// passport.deserializeUser((user, done) => {
+//   return done(null);
+// });
+
+// passport.use(
+//   new DiscordStrategy(
+//     {
+//       clientID: process.env.DISCORD_CLIENT_ID as string,
+//       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+//       callbackURL: '/auth/discord/callback',
+//       scope: discordScopes,
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//       return cb(null, profile);
+//     }
+//   )
+// );
 
 const PORT = process.env.PORT || 3002;
 
--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -1,5 +1,46 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
+import passport from 'passport';
+import DiscordStrategy from 'passport-discord';
+import * as dotenv from 'dotenv';
+dotenv.config();
 const authRouter = express.Router();
+console.log(process.env.DISCORD_CLIENT_ID);
+console.log(process.env.DISCORD_CLIENT_SECRET);
+
+// const discordScopes = ['identify', 'email', 'guilds', 'guilds.join'];
+
+// passport.serializeUser((user, done) => {
+//   return done(null, user);
+// });
+
+// passport.deserializeUser((user, done) => {
+//   return done(null);
+// });
+
+// passport.use(
+//   new DiscordStrategy(
+//     {
+//       clientID: process.env.DISCORD_CLIENT_ID as string,
+//       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+//       callbackURL: '/auth/discord/callback',
+//       scope: discordScopes,
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//       return cb(null, profile);
+//     }
+//   )
+// );
+
+authRouter.get('/discord', passport.authenticate('discord'));
+authRouter.get(
+  '/discord/callback',
+  passport.authenticate('discord', {
+    failureRedirect: '/login',
+  }),
+  function (req: Request, res: Response) {
+    res.redirect('/dashboard');
+  }
+);
 
 authRouter.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello From /auth Route!' });
