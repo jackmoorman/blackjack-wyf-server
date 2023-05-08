35b17e99655380edb8e64c49a9abe2ec9fd89e51
johnmoormaniii <johnmoormaniii@gmail.com>
2023-05-08 13:30:36 -0400

    Passport working with Google and Discord

--- server/index.ts
+++ server/index.ts
@@ -1,61 +1,95 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import * as dotenv from 'dotenv';
-dotenv.config();
 import cors from 'cors';
 import passport from 'passport';
-import DiscordStrategy from 'passport-discord';
 import session from 'express-session';
-
-// route imports
-import authRouter from './routes/authRouter.js';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as DiscordStrategy } from 'passport-discord';
 import apiRouter from './routes/apiRouter.js';
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
 const app: Express = express();
-app.use(cors());
 app.use(express.json());
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(cors());
 app.use(
   session({
-    secret: process.env.SESSION_SECRET as string,
+    secret: process.env.SESSION_SECRET!,
     resave: true,
     saveUninitialized: true,
-    cookie: {
-      sameSite: 'none',
-      secure: true,
-      maxAge: 1000 * 60 * 60 * 24, // One day
-    },
   })
 );
 
-// const discordScopes = ['identify', 'email', 'guilds', 'guilds.join'];
+app.use(passport.initialize());
+app.use(passport.session());
 
-// passport.serializeUser((user, done) => {
-//   return done(null, user);
-// });
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
 
-// passport.deserializeUser((user, done) => {
-//   return done(null);
-// });
+passport.deserializeUser((user, done) => {
+  console.log('deserializeUser', user);
+  return done(null);
+});
 
-// passport.use(
-//   new DiscordStrategy(
-//     {
-//       clientID: process.env.DISCORD_CLIENT_ID as string,
-//       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
-//       callbackURL: '/auth/discord/callback',
-//       scope: discordScopes,
-//     },
-//     function (accessToken, refreshToken, profile, cb) {
-//       return cb(null, profile);
-//     }
-//   )
-// );
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: '/auth/google/callback',
+    },
+    function (_, __, profile, cb) {
+      console.log('CallbackURL reached', profile);
+      return cb(null, profile);
+    }
+  )
+);
 
-const PORT = process.env.PORT || 3002;
+passport.use(
+  new DiscordStrategy(
+    {
+      clientID: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      callbackURL: '/auth/discord/callback',
+      scope: ['identify', 'email', 'guilds', 'guilds.join'],
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log('Discord callback URL: ', profile);
+      return cb(null, profile);
+    }
+  )
+);
 
-app.use('/auth', authRouter);
-app.use('/api', apiRouter);
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: 'http://localhost:3000',
+    session: true,
+  }),
+  function (req, res) {
+    console.log('Google callback reached at /auth/google/callback');
+    res.status(200).json({ message: 'Successfully logged in with Google' });
+  }
+);
+
+app.get('/auth/discord', passport.authenticate('discord'));
+app.get(
+  '/auth/discord/callback',
+  passport.authenticate('discord', {
+    failureRedirect: '/',
+  }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello From Root Route!' });
